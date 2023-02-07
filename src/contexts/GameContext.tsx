47943ef30d0027175e1649c231a6c86c61114e29
Martin Asser Hansen <mail@maasha.dk>
2023-02-07 21:35:54 +0100

    Generation of codeToGuess ok

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode, useMemo } from "react";
 import { COLUMNS, ROWS } from "../static/board-dimensions.const";
+import { Colors } from "../static/colors.enum";
 
 export type GameContextType = {
   rows: number;
@@ -12,7 +13,24 @@ type GameContextProviderProps = {
   children: ReactNode;
 };
 
+function createCode(allowDuplicates = false): string[] {
+  const colors = Object.keys(Colors);
+  const code: string[] = [];
+
+  while (code.length < COLUMNS) {
+    const color = colors[Math.floor(Math.random() * COLUMNS)];
+    if (!allowDuplicates && code.includes(color)) continue;
+    code.push(color);
+  }
+
+  return code;
+}
+
 export function GameContextProvider({ children }: GameContextProviderProps) {
+  const codeToGuess = createCode();
+
+  console.log({ codeToGuess });
+
   const boardValue = useMemo(() => {
     return {
       rows: ROWS,
