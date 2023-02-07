dfabae32d3e8749844ff78aeb4a740ed4110909c
Martin Asser Hansen <mail@maasha.dk>
2023-02-07 13:35:48 +0100

    Fixing BoardContextType

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,15 +1,14 @@
 import { useContext } from "react";
-import { BoardContext } from "../contexts/BoardContext";
+import { BoardContext, BoardContextType } from "../contexts/BoardContext";
 import Row from "./Row";
 
 export default function Board() {
-  const { rows } = useContext(BoardContext);
-
-  console.log("HER", rows);
+  const { rows } = useContext(BoardContext) as BoardContextType;
   const rowList = [];
 
   for (let i = 0; i < rows; i += 1) {
     rowList.push(<Row key={i} />);
   }
+
   return <div className="mastermind-board">{rowList}</div>;
 }
