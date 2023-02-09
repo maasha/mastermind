9d3a1da36bed8d9ac8aabfb569108a1491809d3e
Martin Asser Hansen <mail@maasha.dk>
2023-02-09 16:40:15 +0100

    Footer styled

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,16 @@ import { GameContext } from "../contexts/GameContext";
 export default function Footer() {
   const { gameWon, gameOver } = useContext(GameContext);
 
+  const msgClass = "text-xl flex justify-center font-bold";
+
   return (
     <div>
-      {gameWon && <div>Game Won - refresh to play again</div>}
-      {gameOver && <div>Game Over - refresh to play again</div>}
+      {gameWon && (
+        <div className={msgClass}>Game Won - refresh to play again</div>
+      )}
+      {gameOver && (
+        <div className={msgClass}>Game Over - refresh to play again</div>
+      )}
     </div>
   );
 }
